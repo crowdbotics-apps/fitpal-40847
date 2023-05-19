200a4a6e02f246279e7e027f68e978b6df808ede
Crowdbotics <team@crowdbotics.com>
2023-05-19 16:41:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fitpal-40847",
+  url: "https://fitpal-40847.botics.co",
+  api: "https://fitpal-40847.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
